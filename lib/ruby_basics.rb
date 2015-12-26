6e5ae49fca1d6e0cb8fe8d8b8d156c87c94d7293
BettinaP <bettinaprophete@gmail.com>
2015-12-25 22:43:11 -0500

    completed todo ruby basics

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,38 @@
 def division(num1, num2)
- 
+  num1 = 42
+  num2 = 7
+  puts "num1 / num2"
+  num1 / num2 
 end
 
-def assign_variable(value)
+def assign_variable(value= Bob)
+  puts "#{value}"
+  "#{value}"
+end
+
+def argue(phrase)
+  phrase = "I'm right and you are wrong!"
+  puts "#{phrase}"
+  "#{phrase}"
   
 end
 
-def argue
-  
-end
-
-def greeting
-
+def greeting(greeting= "Hi there, ", name= Bobby!)
+  puts "#{greeting}, #{name}"
+  "#{greeting}, #{name}"
 end
 
 def return_a_value
-
+  puts "Nice"
+  "Nice"
 end
 
 def last_evaluated_value
-
+  puts "expert"
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping= "cheese")
+  puts "#{topping}"
+  "#{topping}"
 end
